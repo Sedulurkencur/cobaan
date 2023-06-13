f31dea3d9301e1b630662f30a1e68c2a6eef751c
Sedulurkencur <115388889+Sedulurkencur@users.noreply.github.com>
2023-06-13 20:54:39 +0700

    Delete kode.php

--- kode.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "\nSEDANG RUN\n";
-jembut :
-$kodeku = substr(str_shuffle(str_repeat("2346789abcdefghjklmnpqrtuvwxyz", 5)), 0, 10);
-file_put_contents("KODEKU.txt",$kodeku."\n",FILE_APPEND);
-goto jembut;
